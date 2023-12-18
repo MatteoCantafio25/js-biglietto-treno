4466e45efd9a9ee3a88a12f7872780abeea11805
MatteoCantafio25 <mattyteo004@gmail.com>
2023-12-18 19:59:24 +0100

    added prompts

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+//---------------------------------//
+// MI ASSICURO CHE IL FOGLIO JS COMUNICHI CON IL FOGLIO HTML
+
 console.log("JS OK")
+
+//---------------------------------//
+// CREO UNA COSTANTE FIRSTNAME A CUI DARA' VALORE IL PASSEGGERO
+
+const km = prompt("Quanti Km farai in questo viaggio?")
+
+console.log(km)
+
+//---------------------------------//
+// CREO UNA COSTANTE ETA' A CUI DARA' VALORE IL PASSEGGERO
+
+const age = prompt("Quanti anni hai?")
+
+console.log(age)
+
+

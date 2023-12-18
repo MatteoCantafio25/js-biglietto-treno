eb446eb098d0a2be3b917176ed5a96d3cef590ea
MatteoCantafio25 <mattyteo004@gmail.com>
2023-12-18 20:54:58 +0100

    script completed

--- script.js
+++ script.js
@@ -6,15 +6,62 @@ console.log("JS OK")
 //---------------------------------//
 // CREO UNA COSTANTE FIRSTNAME A CUI DARA' VALORE IL PASSEGGERO
 
-const km = prompt("Quanti Km farai in questo viaggio?")
+const km = parseInt(prompt("Quanti Km farai in questo viaggio?"))
 
 console.log(km)
 
 //---------------------------------//
 // CREO UNA COSTANTE ETA' A CUI DARA' VALORE IL PASSEGGERO
 
-const age = prompt("Quanti anni hai?")
+let age = parseInt(prompt("Quanti anni hai?"))
 
 console.log(age)
+
+//---------------------------------//
+// CREO UNA COSTANTE MONEY CON UN VALORE PREDEFINITO
+
+const money = 0.21
+
+console.log(money)
+
+//---------------------------------//
+// CREO UNA COSTANTE RESULT CHE MOLTIPLICHERA' SOLDI * NUMERO DI KILOMETRI
+
+let result = money * km
+
+console.log(result)
+
+
+//---------------------------------//
+// CREO UNA COSTANTE MINDISCOUNT CHE ANDRA' A MODIFICARE IL RISULTATO PER 20%
+
+const minDiscount = result * 20 / 100
+
+//---------------------------------//
+// CREO UNA COSTANTE MAXDISCOUNT CHE ANDRA' A MODIFICARE IL RISULTATO PER 40%
+
+const maxDiscount = result * 40 / 100
+
+//---------------------------------//
+// SE L'ETA' E' MINORE O UGUALE DI 18 SI PRENDE IL RISULTATO MENO IL MINDISCOUNT
+
+if (age <= 18) {
+
+    result = result - minDiscount
+
+    console.log(result)
+}
+
+//---------------------------------//
+// SE L'ETA' E' MAGGIORE O UGUALE DI 65 SI PRENDE IL RISULTATO MENO IL MAXDISCOUNT
+
+else if (age >= 65) {
+    result = result - maxDiscount
+
+    console.log(result)
+}
+
+
+
 
 
